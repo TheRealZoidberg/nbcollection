c940116f5702e261210fde8d1a32860780573086
Michael Gough <gough@stsci.edu>
2021-06-29 15:41:55 -0400

    updates

--- nbcollection/ci/generate_ci_environment/utils.py
+++ nbcollection/ci/generate_ci_environment/utils.py
@@ -39,16 +39,25 @@ def gen_ci_env(jobs: typing.List[BuildJob], ci_env: CIEnvironment, project_path:
         ])
         job['steps'][2]['run']['name'] = f'Build {job_name} notebooks'
         job['steps'][3]['store_artifacts']['path'] = SCANNER_ARTIFACT_DEST_DIR
-        job['steps'][4]['run']['command'] = ' '.join([
-            'pip install -r',
-            f'{build_job.collection.name}/{build_job.category.name}/pre-requirements.txt',
-            ';',
-            'pip install -r',
-            f'{build_job.collection.name}/{build_job.category.name}/requirements.txt',
-            ';',
-            'python -m pytest --nbval ',
-            f'{build_job.collection.name}/{build_job.category.name}',
-        ])
+        if os.path.isfile(f'{build_job.collection.name}/{build_job.category.name}/pre-requirements.txt'):
+            job['steps'][4]['run']['command'] = ' '.join([
+                'pip install -r',
+                f'{build_job.collection.name}/{build_job.category.name}/pre-requirements.txt',
+                ';',
+                'pip install -r',
+                f'{build_job.collection.name}/{build_job.category.name}/requirements.txt',
+                ';',
+                'python -m pytest --nbval ',
+                f'{build_job.collection.name}/{build_job.category.name}',
+            ])
+        else:
+            job['steps'][4]['run']['command'] = ' '.join([
+                'pip install -r',
+                f'{build_job.collection.name}/{build_job.category.name}/requirements.txt',
+                ';',
+                'python -m pytest --nbval ',
+                f'{build_job.collection.name}/{build_job.category.name}',
+            ])
         config['jobs'][job_name] = job
         config['workflows'][NBCOLLECTION_WORKFLOW_NAME]['jobs'].append(job_name)
         if not build_job.collection.name in formatted_collections:
